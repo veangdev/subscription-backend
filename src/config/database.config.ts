55555bd17ee06e6e6aae120f4f8d9a2104850cbf
VEANGDEV6 <kroh.veang@realwat.net>
2026-01-22 00:23:25 +0700

    Fix: bind NestJS to 0.0.0.0 for Cloud Run2

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,8 +6,10 @@ dotenv.config();
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT || 5432),
+  port: Number(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  autoLoadEntities: true,
+  synchronize: false,
 };
