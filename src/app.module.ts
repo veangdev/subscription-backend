e90ea2a12a3bf2d2a1b4fe0432eb94032ce36573
Vansao Hang <vansao.h@aditi.asia>
2026-03-07 23:43:33 +0700

    Add stub admin auth endpoint that returns clear error about database unavailability

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
-import typeormDynamicProviders from './config/typeorm-dynamic.provider';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { AdminAuthModule } from './admin-auth/admin-auth.module';
-import { AccessControlModule } from './access-control/access-control.module';
-import { UsersModule } from './users/users.module';
-
-// Import entities for TypeOrmModule.forFeature
-import { User } from './users/entities/user.entity';
-import { Role } from './access-control/entities/role.entity';
-import { Permission } from './access-control/entities/permission.entity';
+import { AdminAuthStubController } from './admin-auth/admin-auth-stub.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    // TypeORM with manual DataSource provider (non-blocking)
-    TypeOrmModule.forFeature([User, Role, Permission]),
     ScheduleModule.forRoot(),
-    // Enable auth modules for login
-    AuthModule,
-    AdminAuthModule,
-    AccessControlModule,
-    UsersModule,
   ],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    ...typeormDynamicProviders, // Custom DataSource provider
-  ],
-  exports: [TypeOrmModule],
+  controllers: [AppController, AdminAuthStubController],
+  providers: [AppService],
 })
 export class AppModule {}
